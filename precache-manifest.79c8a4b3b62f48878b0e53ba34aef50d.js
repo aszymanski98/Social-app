5f7352ab0f2a2dc3d998ad5907ea920b10301cf7
aszymanski98 <adam.szymanski@protonmail.com>
2020-09-16 18:42:01 +0200

    Updates

---
+++ precache-manifest.79c8a4b3b62f48878b0e53ba34aef50d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44edf13c4a8aaa2cde04bce10738e7fb",
+    "url": "/Social-app-ReactJS/index.html"
+  },
+  {
+    "revision": "cf1dbf26b765fcf7b0e8",
+    "url": "/Social-app-ReactJS/static/css/main.47c7cb79.chunk.css"
+  },
+  {
+    "revision": "f9ac28434939f643755c",
+    "url": "/Social-app-ReactJS/static/js/2.c62eeea1.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Social-app-ReactJS/static/js/2.c62eeea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1dbf26b765fcf7b0e8",
+    "url": "/Social-app-ReactJS/static/js/main.4bd90cc3.chunk.js"
+  },
+  {
+    "revision": "e114f49f7920fd9c9220",
+    "url": "/Social-app-ReactJS/static/js/runtime-main.99908374.js"
+  },
+  {
+    "revision": "f73f97ff9aa2b04ccde8598887d928d0",
+    "url": "/Social-app-ReactJS/static/media/Loader.f73f97ff.gif"
+  }
+]);
